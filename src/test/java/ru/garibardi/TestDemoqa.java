a08aee3e2530a92267b7035c14f5d42dc1cc6b6d
garibardi1991 <garibardi@mail.ru>
2023-01-29 16:40:36 +0300

    init commit

--- src/test/java/ru/garibardi/TestDemoqa.java
+++ src/test/java/ru/garibardi/TestDemoqa.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Browsers.EDGE;
 import static com.codeborne.selenide.Condition.appear;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -16,6 +17,8 @@ public class TestDemoqa {
     static void configure () {
         Configuration.browser = EDGE;
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
     }
 
 
@@ -25,21 +28,28 @@ public class TestDemoqa {
         $("#firstName").setValue ("Igor");
         $("#lastName").setValue ("Trubikhov");
         $("#userEmail").setValue ("garibardi@mail.ru");
-        $("#gender-radio-1").click();
-        $("#gender-radio-2").click();
-        $("#gender-radio-3").click();
+        $("#genterWrapper").$(byText("Male")).click();
+        $("#genterWrapper").$(byText("Female")).click();
+        $("#genterWrapper").$(byText("Other")).click();
+        $("#genterWrapper").$(byText("Male")).click();
         $("#userNumber").setValue ("+79523811047");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").click ();
-        $(".react-datepicker__month-select").selectOptionContainingText("3");
-        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__month-select").selectOptionContainingText("April");
         $(".react-datepicker__year-select").selectOptionContainingText("1991");
-        $(".react-datepicker__day react-datepicker__day--025").click();
-        $(".subjects-auto-complete__placeholder css-1wa3eu0-placeholder").setValue("Volgograd");
-        $("#hobbies-checkbox-1").click();
-        $("#hobbies-checkbox-2").click();
-        $("#hobbies-checkbox-3").click();
+        $(".react-datepicker__day--025").click();
+        $("#subjectsInput").setValue("Volgograd").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
 
+        $("#uploadPicture").uploadFromClasspath("1.jpeg");
+
+        $("#currentAddress").setValue("2-y Karavannay 15-4");
+
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("Haryana")).click();
+
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Karnal")).click();
+        $("#submit").click();
 
     }
 }
