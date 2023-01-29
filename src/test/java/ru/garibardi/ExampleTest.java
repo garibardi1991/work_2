67cfe1f8e9a263456cca743f1006dc9c8623ea25
garibardi1991 <garibardi@mail.ru>
2023-01-29 12:35:07 +0300

    init commit

--- src/test/java/ru/garibardi/ExampleTest.java
+++ src/test/java/ru/garibardi/ExampleTest.java
@@ -1,7 +1,9 @@
 package ru.garibardi;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Browsers.EDGE;
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selenide.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -23,6 +25,7 @@ public class ExampleTest {
     }
     @Test
     void openeshop() {
+        Configuration.browser = EDGE;
         open("https://eshoprzd.ru/home");
         $("#login-btn").click();
         $("[ng-click*='showLoginForm']").should(appear);
