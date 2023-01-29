4fc4d10d39062f090457d6c5a4b850c1446a0679
garibardi1991 <garibardi@mail.ru>
2023-01-29 17:04:53 +0300

    init commit

--- src/test/java/ru/garibardi/TestDemoqa.java
+++ src/test/java/ru/garibardi/TestDemoqa.java
@@ -1,15 +1,13 @@
 package ru.garibardi;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Browsers.EDGE;
-import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class TestDemoqa {
 
@@ -19,6 +17,7 @@ public class TestDemoqa {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
     }
 
 
@@ -32,24 +31,25 @@ public class TestDemoqa {
         $("#genterWrapper").$(byText("Female")).click();
         $("#genterWrapper").$(byText("Other")).click();
         $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue ("+79523811047");
+        $("#userNumber").setValue ("8952381104");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionContainingText("April");
         $(".react-datepicker__year-select").selectOptionContainingText("1991");
         $(".react-datepicker__day--025").click();
-        $("#subjectsInput").setValue("Volgograd").pressEnter();
+        $("#subjectsInput").setValue("ma");
+        $(byText("Maths")).click();
         $("#hobbiesWrapper").$(byText("Sports")).click();
 
-        $("#uploadPicture").uploadFromClasspath("1.jpeg");
+        $("#uploadPicture").uploadFromClasspath("files/IMG_20211118_183446.jpg");
 
-        $("#currentAddress").setValue("2-y Karavannay 15-4");
-
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("Haryana")).click();
-
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Karnal")).click();
-        $("#submit").click();
+//        $("#currentAddress").setValue("2-y Karavannay 15-4");
+//
+//        $("#state").click();
+//        $("#stateCity-wrapper").$(byText("Haryana")).click();
+//
+//        $("#city").click();
+//        $("#stateCity-wrapper").$(byText("Karnal")).click();
+//        $("#submit").click();
 
     }
 }
