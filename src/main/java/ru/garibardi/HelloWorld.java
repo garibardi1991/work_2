23d2fba46d9f67eadb21ef787bb398cf3c3b07f0
Igor pol <garibardi@mail.ru>
2023-02-01 21:07:27 +0300

    import

---
+++ src/main/java/ru/garibardi/HelloWorld.java
@@ -0,0 +1,13 @@
+package ru.garibardi;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        String stringThatShouldBeDisplayedInConsole = "Hello World";
+
+        boolean flag = true; boolean flag0 = false;
+
+        System.out.println(stringThatShouldBeDisplayedInConsole);
+    }
+
+}
