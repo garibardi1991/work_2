87ac4d8e33bcf98bb692266d6494543ad2c1701a
Igor pol <garibardi@mail.ru>
2023-02-01 22:15:51 +0300

    import

--- src/main/java/ru/garibardi/HelloWorld.java
+++ src/main/java/ru/garibardi/HelloWorld.java
@@ -5,9 +5,33 @@ public class HelloWorld {
     public static void main(String[] args) {
         String stringThatShouldBeDisplayedInConsole = "Hello World";
 
+        // mast have
         boolean flag = true; boolean flag0 = false;
+        byte aByte; // 8 бит
+        short aShort; // 16 бит
 
-        System.out.println(stringThatShouldBeDisplayedInConsole);
+        // mast have
+        int aInt; // 32 бит
+
+        long aLong; // 64 бита
+
+        float aFloat;
+
+        // mast have
+        double aDouble = 1.8;
+
+        char aChar = '1';
+
+        // Операторы + - * % /
+        // Операторы условий < > <= >= == !=
+        // Операторы логические && ||
+
+        int a = 10;
+        int b = 15;
+
+
+
+        System.out.println(a-b);
     }
 
 }
