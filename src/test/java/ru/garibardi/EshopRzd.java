e08df80d51f90d329600111fa3665846f9691856
Igor pol <garibardi@mail.ru>
2023-02-02 22:49:08 +0300

    import new

--- src/test/java/ru/garibardi/EshopRzd.java
+++ src/test/java/ru/garibardi/EshopRzd.java
@@ -5,39 +5,24 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Browsers.EDGE;
+import static com.codeborne.selenide.Browsers.CHROME;
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selenide.*;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ExampleTest {
+public class EshopRzd {
 
     @BeforeAll
     static void configure () {
-        Configuration.browser = EDGE;
+        Configuration.browser = CHROME;
     }
 
     @AfterAll
     static void terDown() {
-        Configuration.browser = EDGE;
+        Configuration.browser = CHROME;
     }
 
     @Test
-    void test1() {
-        assertTrue(true);
-    }
-
-    @Test
-    void test2() {
-        assertTrue(true);
-    }
-
-    @Test
-    void test3() {
-        assertTrue(true);
-    }
-    @Test
-    void openeshop() {
+    void openeshop () {
         open("https://eshoprzd.ru/home");
         $("#login-btn").click();
         $("[ng-click*='showLoginForm']").should(appear);
