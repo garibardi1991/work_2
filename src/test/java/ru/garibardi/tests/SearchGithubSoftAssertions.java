21e2790a67a598e3a4896a6b2df25dd5dc9c340a
Igor pol <garibardi@mail.ru>
2023-02-26 15:47:22 +0300

    added code

---
+++ src/test/java/ru/garibardi/tests/SearchGithubSoftAssertions.java
@@ -0,0 +1,40 @@
+package ru.garibardi.tests;
+
+import com.codeborne.selenide.Selectors;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SearchGithubSoftAssertions {
+
+
+
+    @Test
+    void SearchJUnit5 () {
+        open("https://github.com/");
+        $("[data-scoped-placeholder=Search]").setValue("selenide").pressEnter();
+        $$(".repo-list li").first().$("a").click();
+        $("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(byText("SoftAssertions")).click();
+        $(".gollum-markdown-content").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+
+    @Test
+    void SearchSelenide () {
+        open("https://github.com/selenide/selenide");
+        $(".Layout-sidebar").$(byText("Contributors"))
+                .ancestor(".BorderGrid-cell").$$("ul li")
+                .first().hover();
+
+        $$(".Popover-message").findBy(visible).shouldHave(text("Andrei Solntsev"));
+
+
+    }
+
+}
