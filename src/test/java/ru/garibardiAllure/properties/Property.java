3714ae9dde1e44956351078bd9dd7385bf0fb768
Igor pol <garibardi@mail.ru>
2023-03-08 14:15:57 +0300

    added code

---
+++ src/test/java/ru/garibardiAllure/properties/Property.java
@@ -0,0 +1,19 @@
+package ru.garibardiAllure.properties;
+
+public class Property {
+    public static String browser() {
+        return System.getProperty("browser", "chrome");
+    }
+
+    public static String browserVersion() {
+        return System.getProperty("browserVersion", "chrome");
+    }
+
+    public static String browserSize() {
+        return System.getProperty("browserSize", "1920x1080");
+    }
+
+    public static String remoteUrl() {
+        return System.getProperty("remoteUrl", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+    }
+}
