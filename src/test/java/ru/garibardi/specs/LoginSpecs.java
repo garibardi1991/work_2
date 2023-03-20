0401705364dee2cb9b85536f8bba2b759d878ae7
Igor pol <garibardi@mail.ru>
2023-03-20 23:05:07 +0300

    added code

--- src/test/java/ru/garibardi/specs/LoginSpecs.java
+++ src/test/java/ru/garibardi/specs/LoginSpecs.java
@@ -4,12 +4,12 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
-import static helpers.CustomApiListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
 import static io.restassured.filter.log.LogDetail.BODY;
 import static io.restassured.filter.log.LogDetail.STATUS;
 import static io.restassured.http.ContentType.JSON;
 import static org.hamcrest.Matchers.notNullValue;
+import static ru.garibardi.helpers.CustomApiListener.withCustomTemplates;
 
 // todo move to request & response specs classes
 public class LoginSpecs {
